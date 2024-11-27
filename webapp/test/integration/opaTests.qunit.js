9ef8d3863d9811b67d90833f62fb7e7669f3c3fa
Kanwaljit <56593511+kanwaljit8549@users.noreply.github.com>
2024-11-27 16:09:01 +0530

    Delete webapp directory

--- webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["gwsp1tempproject/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"gw_sp1_temp_project/test/unit/controller/GW_SP1_TEMP_View.controller"
-], function () {
-	"use strict";
-});
